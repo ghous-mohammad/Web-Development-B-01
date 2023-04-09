cc3287c49cceb83f23b88082b78d9e9ac930e591
ghous-mohammad <ghosmohammad247@gmail.com>
2023-04-09 23:35:37 +0500

    javascript first lecture learned history of js , data types etc..

---
+++ Javascript/Introduction/app.js
@@ -0,0 +1,12 @@
+ // alert ("helo world")
+
+   var meranam = "ghous"  //string
+   var mynum = 4658 //number
+
+  console.log(meranam)
+  console.log(mynum)
+
+  meranam = "bilal"
+  console.log(meranam)
+
+  document.write(meranam)
